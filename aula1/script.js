4e8c11d4405c0942de7482c4ab97ddbd8e756f1e
Joao Victor <jvmlyra@gmail.com>
2024-08-15 11:15:45 -0300

    Desafio 19

---
+++ aula1/script.js
@@ -0,0 +1,26 @@
+console.log("Hello World")
+
+console.log(typeof 5);
+console.log(typeof 12.7);
+console.log(typeof -123);
+
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(typeof NaN);
+
+
+console.log(typeof 'Teste');
+console.log(typeof "Infinity");
+console.log(typeof Infinity);
+console.log("O " + "nome " + "João");
+console.log(`A soma de 5 + 3 é ${5+3}`);
+console.log(`A soma de 2 + 2 é ${2+2}`);
+
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof (10 > 2));
+
+console.log(5 == 4);
+console.log('Matheus' != 'Matheus');
+console.log(3 === '3');
+console.log(3 === 3);
---
+++ desafio-1/script.js
@@ -0,0 +1,3 @@
+console.log("Aspas duplas");
+console.log('Aspas simples');
+console.log(`Literals, 2 + 2 é ${2+2}`);
