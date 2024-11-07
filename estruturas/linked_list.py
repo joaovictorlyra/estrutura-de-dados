31528adff6a74efe282b4a58c7963511a94ad332
Joao Victor <jvmlyra@gmail.com>
2024-11-07 17:09:08 -0300

    Node and allocation

---
+++ estruturas/linked_list.py
@@ -0,0 +1,19 @@
+# sequencial = []
+# sequencial.append(7)
+
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+
+    def append(self, elem):
+        if self.head:
+            pass
+        else:
+            Node()
+
+lista = LinkedList()
+
