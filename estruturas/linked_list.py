7f2f411744f30d9306bb092d2df1541106ea667f
Joao Victor <jvmlyra@gmail.com>
2024-11-15 15:08:56 -0300

    Python café #6

--- estruturas/linked_list.py
+++ estruturas/linked_list.py
@@ -1,17 +1,14 @@
-# sequencial = []
-# sequencial.append(7)
-
 from node import Node
+
 
 class LinkedList:
     def __init__(self):
         self.head = None
         self._size = 0
 
-
     def append(self, elem):
         if self.head:
-            # Inserção quando a lista já possui elementos
+            # inserção quando a lista já possui elementos
             pointer = self.head
             while(pointer.next):
                 pointer = pointer.next
@@ -19,50 +16,40 @@ class LinkedList:
         else:
             # primeira inserção
             self.head = Node(elem)
-        self._size += 1
+        self._size = self._size + 1
 
-    
     def __len__(self):
-        """"Retorna o tamanho da lista"""
-        return self._size 
+        """Retorna o tamanho da lista"""
+        return self._size
 
+    def _getnode(self, index):
+        pointer = self.head
+        for i in range(index):
+            if pointer:
+                pointer = pointer.next
+            else:
+                raise IndexError("list index out of range") #return None
+        return pointer
 
-    def get(self, index):
-
-        pass
-        
-    
     def set(self, index, elem):
+        # lista.set(5, 9)
         pass
-
 
     def __getitem__(self, index):
         # a = lista[6]
-        pointer = self.head
-        for i in range(index):
-            if pointer:
-                pointer = pointer.next
-            else:
-                raise IndexError("list index out of range")
+        pointer = self._getnode(index)
         if pointer:
             return pointer.data
         else:
-             raise IndexError("list index out of range")
-
-
+            raise IndexError("list index out of range")
 
     def __setitem__(self, index, elem):
         # lista[5] = 9
-        pointer = self.head
-        for i in range(index):
-            if pointer:
-                pointer = pointer.next
-            else:
-                raise IndexError("list index out of range")
+        pointer = self._getnode(index)
         if pointer:
             pointer.data = elem
         else:
-             raise IndexError("list index out of range")
+            raise IndexError("list index out of range")
 
     def index(self, elem):
         """Retorna o índice do elem na lista"""
@@ -71,11 +58,58 @@ class LinkedList:
         while(pointer):
             if pointer.data == elem:
                 return i
-            else:
+            pointer = pointer.next
+            i = i+1
+        raise ValueError("{} is not in list".format(elem))
+
+    def insert(self, index, elem):
+        node = Node(elem)
+        if index == 0:
+            node.next = self.head
+            self.head = node
+        else:
+            pointer = self._getnode(index-1)
+            node.next = pointer.next
+            pointer.next = node
+        self._size = self._size + 1
+
+    def remove(self, elem):
+        if self.head == None:
+            raise ValueError("{} is not in list".format(elem))
+        elif self.head.data == elem:
+            self.head = self.head.next
+            self._size = self._size - 1
+            return True
+        else:
+            ancestor = self.head
+            pointer = self.head.next
+            while(pointer):
+                if pointer.data == elem:
+                    ancestor.next = pointer.next
+                    pointer.next = None
+                    self._size = self._size - 1
+                    return True
+                ancestor = pointer
                 pointer = pointer.next
-                i += 1
-        raise ValueError ("{} is not in list".format(elem))
+        raise ValueError("{} is not in list".format(elem))
 
+    def __repr__(self):
+        r = ""
+        pointer = self.head
+        while(pointer):
+            r = r + str(pointer.data) + "->"
+            pointer = pointer.next
+        return r
 
-lista = LinkedList()
-lista.append(7)
+    def __str__(self):
+        return self.__repr__()
+
+if __name__ == '__main__':
+    # sequencial = []
+    # sequencial.append(7)
+    lista = LinkedList()
+    lista.append(7)
+    lista.append(80)
+    lista.append(56)
+    lista.append(32)
+    lista.append(17)
